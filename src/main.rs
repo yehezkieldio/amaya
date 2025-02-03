fe754c8ce5eb6ef8052eda40ac46035e4c48dea3
Yehezkiel Dio Sinolungan <yehezkieldio35@gmail.com>
2025-02-03 15:38:08 +0800

    feat: implement basic registry and cli

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,20 @@ pub mod error;
 pub mod providers;
 pub mod registry;
 
-fn main() {
-    println!("Hello, world!");
+use args::CLI;
+use clap::Parser;
+use providers::biome::BiomeProvider;
+use registry::AmarisRegistry;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let cli: CLI = CLI::parse();
+
+    let mut registry: AmarisRegistry = AmarisRegistry::new();
+    registry.register(BiomeProvider);
+
+    match cli.command.execute(&registry).await {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e),
+    }
 }
